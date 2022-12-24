9fe5d726e04dc8273a3a63d7c121c68c8a991c3e
amrxt <amrxt11@outlook.com>
2022-12-24 23:07:36 +0530

    Add basic functions and operate() function

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-const xyz = "kjdnkfc";
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(a, operator, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      return divide(a, b);
+      break;
+    case "-":
+      return subtract(a, b);
+      break;
+    case "X":
+      return multiply(a, b);
+      break;
+    case "/":
+      return divide(a, b);
+      break;
+    default:
+      return;
+  }
+}
