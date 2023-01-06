503d7eb702bcbd0461d6ee685e9b326af59a5ff6
amrxt <amrxt11@outlook.com>
2023-01-06 20:24:49 +0530

    Make the equal button work

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function operate(a, operator, b) {
   b = Number(b);
   switch (operator) {
     case "+":
-      return divide(a, b);
+      return add(a, b);
       break;
     case "-":
       return subtract(a, b);
@@ -46,6 +46,8 @@ const ac = document.querySelector(".ac");
 ac.addEventListener("click", () => {
   currentOperand = "";
   primaryScreen.textContent = currentOperand;
+  prevOperand = "";
+  secondaryScreen.textContent = prevOperand;
 });
 
 const del = document.querySelector(".del");
@@ -53,7 +55,6 @@ del.addEventListener("click", () => {
   currentOperand = currentOperand.slice(0, currentOperand.length - 1);
   primaryScreen.textContent = currentOperand;
 });
-const equal = document.querySelector(".equal");
 
 const numberBtns = document.querySelectorAll(".num");
 numberBtns.forEach((btn) => btn.addEventListener("click", transferVal));
@@ -70,9 +71,25 @@ function calculate(e) {
     secondaryScreen.textContent = prevOperand + " " + operator;
     return;
   }
+  if (prevOperand && currentOperand) {
+    operator = this.textContent;
+    secondaryScreen.textContent = prevOperand + " " + operator;
+    currentOperand = "";
+    primaryScreen.textContent = currentOperand;
+    return;
+  }
   prevOperand = currentOperand;
   operator = this.textContent;
   secondaryScreen.textContent = prevOperand + " " + operator;
   currentOperand = "";
   primaryScreen.textContent = "";
+}
+
+const equalBtn = document.querySelector(".equal");
+equalBtn.addEventListener("click", equal);
+
+function equal(e) {
+  var x = operate(Number(prevOperand), operator, Number(currentOperand));
+  secondaryScreen.textContent = x;
+  prevOperand = x;
 }
